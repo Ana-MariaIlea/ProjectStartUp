06be1de12902e9f47335b07c3143a4bf319d08ef
Jeroen de Haan <info.jdehaan@gmail.com>
2021-02-09 12:15:22 +0100

    Implemented moving mechanic for the player
    The player can now move and the camera will be controlled by the mouse. Player speed and mouse sensitivity are changeable.

---
+++ Assets/Jeroen TestStuff/CameraFollow.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CameraFollow : MonoBehaviour
+{
+    public float MouseSensitivity = 100f;
+    public Transform playerBody;
+
+    private float xRotation = 0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        trackMouse();
+    }
+
+    private void trackMouse()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * MouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * MouseSensitivity * Time.deltaTime;
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+
+        playerBody.Rotate(Vector3.up * mouseX);
+    }
+}
