557f57545b78e209c2e4ecf4015d86c817855122
Jeroen de Haan <info.jdehaan@gmail.com>
2021-02-10 17:36:57 +0100

    Added health regeneration
    Added health regeneration when player health percentage becomes lower than 5% and regenerates back to 20%. The health regenerates by 1% per 2 seconds. All values are customizable in the editor. Health regeneration percentage needs to be between 0 and 1 so I added a slider and tooltip to make it clear. Also added an error incase that percentage goes below 0 or above 1, just to make sure.

--- Assets/Jeroen TestStuff/Stats/PlayerStats.cs
+++ Assets/Jeroen TestStuff/Stats/PlayerStats.cs
@@ -5,48 +5,56 @@ using UnityEngine;
 public class PlayerStats : CharacterStats
 {
     private float healthPercentage;
+    private float time = 0f;
     [SerializeField] private float regenWaitTime = 2f;
+    [SerializeField][Tooltip("This is the percentage of the max health that will regenerate over time. 0.01 = 1% & 1 = 100%")]
+    [Range(0f, 1f)] private float regenPercentage = 0.01f;
+    [SerializeField] private float regenerateUntil = 20f;
+    [SerializeField] private float regenerateFrom = 5f;
 
     private bool canRegenerate = false;
 
-    // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-        
+        if (regenPercentage < 0f || regenPercentage > 1f)
+            Debug.LogException(new System.Exception("regenPercentage out of range. Value must be between 0 and 1"));
     }
 
     // Update is called once per frame
     void Update()
     {
         regenHealth();
+        // For test purposes
         if (Input.GetKeyDown(KeyCode.T))
         {
-            Debug.Log("Test");
             TakeDamage(5);
         }
     }
-
+    /// <summary>
+    /// This regenerates health when 
+    /// </summary>
     private void regenHealth()
     {
         healthPercentage = (CurrentHealth / MaxHealth) * 100;
 
-        if (healthPercentage > 0 && healthPercentage <= 5)
+        if (healthPercentage > 0f && healthPercentage <= regenerateFrom)
         {
             canRegenerate = true;
-            StartCoroutine(healthRegen());
         }
-        else
-            canRegenerate = false;
-    }
 
-    private IEnumerator healthRegen()
-    {
-        if (canRegenerate)
+        if (healthPercentage < regenerateUntil && canRegenerate)
         {
-            yield return new WaitForSeconds(regenWaitTime);
-            Heal(MaxHealth / 100);
+            time += Time.deltaTime;
+
+            if (time >= regenWaitTime)
+            {
+                time = 0f;
+                Heal(MaxHealth * regenPercentage);
+            }
         }
         else
-            yield break;
+        {
+            canRegenerate = false;
+        }
     }
 }
