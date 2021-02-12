0a7bb6ef1b33585a4934349b46b5ef61675741d7
Ana-Maria Ilea <62722821+Ana-MariaIlea@users.noreply.github.com>
2021-02-12 14:31:14 +0100

    EnemyDies ChangeMaterial

--- Assets/Jeroen TestStuff/Scripts/Stats/EnemyStats.cs
+++ Assets/Jeroen TestStuff/Scripts/Stats/EnemyStats.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class EnemyStats : CharacterStats
 {
-
+    public Material deathMaterial;
     public override void handleDeath()
     {
         Debug.Log(transform.name + " died");
@@ -12,6 +12,7 @@ public class EnemyStats : CharacterStats
         //Change it so that enemy stops moving but the corpse is still in the game 
        GetComponent<EnemyBehaviour>().EnemyDies();
         GetComponent<EnemyBehaviour>().enabled = false;
+        GetComponentInChildren<MeshRenderer>().material = deathMaterial;
         this.enabled = false;
        // Destroy(gameObject);
     }
